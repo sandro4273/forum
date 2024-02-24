c355fab1f8d2f89f54cb2ec556e392ef2781f114
sandro4273 <76450874+sandro4273@users.noreply.github.com>
2024-02-24 17:39:00 +0100

    kleines Frontend das Posts ladet und beim anklicken eines Posts zu dessen Kommentare geht

--- api.py
+++ api.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import db_service
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware( CORSMiddleware,
+                    allow_origins=origins, 
+                    allow_credentials=True, 
+                    allow_methods=["*"], 
+                    allow_headers=["*"])
+
 
 class Post(BaseModel):
     user_id: int
@@ -15,12 +30,17 @@ class Comment(BaseModel):
     content: str
 
 
-@app.get("/post/{post_id}/")
+@app.get("/post/id/{post_id}/")
 async def get_post_by_id(post_id: int):
     return {"result": db_service.get_post_by_id(post_id)}
 
 
-@app.get("/post/{post_id}/comments/")
+@app.get("/post/all/")     # Spöter query dass nur en max. azahl an posts glade werded
+async def get_all_posts():
+    return {"result": db_service.get_all_posts()}
+
+
+@app.get("/post/id/{post_id}/comments/")
 async def get_comments_of_post(post_id: int):
     return {"result": db_service.get_comments_of_post(post_id)}
 
