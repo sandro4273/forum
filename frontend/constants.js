78c8d3c75558da9d0f228ddb5a8e9361995d8096
sandro4273 <76450874+sandro4273@users.noreply.github.com>
2024-02-26 19:18:09 +0100

    Erstellen von Posts vom Frontend aus

---
+++ frontend/constants.js
@@ -0,0 +1,2 @@
+const BACKENDURL = "http://localhost:8000/";
+const FRONTENDURL = "http://localhost:5500/";
--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,3 @@
-const BACKENDURL = "http://localhost:8000/";
-const FRONTENDURL = "http://localhost:5500/";
-
 const postList = document.querySelector("#postList");
 
 async function loadPosts(){     // Spöter mal en max. azahl vo gladene Posts ibaue
@@ -12,7 +9,7 @@ async function loadPosts(){     // Spöter mal en max. azahl vo gladene Posts ib
         const link = document.createElement('a');
         link.setAttribute("href", FRONTENDURL + "frontend/post.html?id=" + postsArray[i]["post_id"]);
         // Neues Element met link zum Post und Kommentär
-        const newElement = document.createElement('li');
+        const newElement = document.createElement('p');
         newElement.appendChild(link).textContent = postsArray[i]["title"];
         postList.append(newElement);
     }
