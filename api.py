ee7e61212afd78c0b63a63991cd9d8234a461e7d
LULL1 <160033744+LULL1@users.noreply.github.com>
2024-03-01 11:40:20 +0100

    test commit

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import db_service
 
-app = FastAPI()
+app = FastAPI() #test
 
 origins = [
     "http://localhost",
