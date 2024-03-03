1e23e91ed11f0f1cb85833850e9fb83dc1e72081
lucafluehler <luca.fluehler@gmx.ch>
2024-03-03 17:50:20 +0100

    Fehlermeldung bei fehlgeschlagener Registrierung, sonst Umleitung zu Login

--- frontend/scripts/signup.js
+++ frontend/scripts/signup.js
@@ -15,8 +15,14 @@ async function submitSignupForm(){
             headers: {"Content-Type" : "application/json"},
             body: JSON.stringify(body),
         });
-    res = await response.json();
-    return response;
+
+    if (response.ok) {
+        window.location.href = "/ProgProjekt_Forum/frontend/pages/login.html";
+    } else {
+        console.error("Error signing up:", response.statusText);
+        document.getElementById("errorMessage").style.display = "block";
+        console.error("Error signing up:", response.statusText);
+    }
 }
 
 window.addEventListener("DOMContentLoaded", onLoad());
