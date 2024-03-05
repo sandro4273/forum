176a956290c7e57ea719b1e601c3702e77d0d068
lucafluehler <luca.fluehler@gmx.ch>
2024-03-05 21:21:51 +0100

    Erstelle Post mit UserID von eingeloggtem User; automatische Weiterleitung zum erstellten Post

--- frontend/scripts/createPost.js
+++ frontend/scripts/createPost.js
@@ -7,23 +7,41 @@ function onLoad(){
 
 async function submitForm(){
     // Titel und Text extrahieren
-    const postTitle = document.forms["createPost"]["postTitle"].value;
-    const postContent = document.forms["createPost"]["postContent"].value;
+    const post_title = document.forms["createPost"]["postTitle"].value;
+    const post_content = document.forms["createPost"]["postContent"].value;
 
-    // API request
-    const body= {   "user_id": 1,   // Account-System noch nicht implementiert
-                    "title": postTitle,
-                    "content": postContent};
-    const response = await fetch(
+    // Retrieve the user_id of the logged in user
+    const auth_token = localStorage.getItem("AuthToken");
+    const user_id_response = await fetch(
+        BACKENDURL + `get_current_user_id/`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${auth_token}`
+        }
+    });
+
+    const user_id = await user_id_response.json();
+
+    // Create post with user_id, title and content
+    const body= {
+        "title": post_title,
+        "content": post_content
+    };
+
+    const create_post_response = await fetch(
         BACKENDURL + "post/create_post/", {
             method: "POST", 
-            headers: {"Content-Type" : "application/json"},
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization": `Bearer ${auth_token}`
+            },
             body: JSON.stringify(body),
         });
-    res = await response.json();
-    return response;
-    
-    // Redirect to post
+
+    let post_id = await create_post_response.json();
+    window.location.href = "/ProgProjekt_Forum/frontend/pages/post.html?id=" + post_id;
+    return post_id;
 }
 
 // Init ausführen wenn Seite geladen ist
