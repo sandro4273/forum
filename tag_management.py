636f6631a382d9f40e0f7ddd070901d3a5d61370
sandro4273 <76450874+sandro4273@users.noreply.github.com>
2024-03-07 14:42:37 +0100

    Backend functionality for tags

---
+++ tag_management.py
@@ -0,0 +1,9 @@
+from db_service import create_tag
+
+def assign_tags_to_post(post_title, post_content):
+    tags = []
+    # TODO: Extract keywords from post
+
+    # TODO: Create tags from keywords
+
+    return tags
