70cc7a5572ea39da7d15cc8c168c40b2132050d1
sandro4273 <76450874+sandro4273@users.noreply.github.com>
2024-03-23 12:14:08 +0100

    Format text in post/comment and editComment input fields

---
+++ frontend/src/quillSettings.js
@@ -0,0 +1,11 @@
+const quillSettings = {
+    modules: {
+        toolbar: [
+            [{ header: [1, 2, false] }],
+            ['bold', 'italic', 'underline'],
+            ['link'],
+            ['clean']
+        ]
+    },
+    theme: 'snow'
+}
