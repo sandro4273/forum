587145de8f9e2c859ac7f087595502b41c2b316a
lucafluehler <131798379+lucafluehler@users.noreply.github.com>
2024-03-23 12:00:42 +0100

    improve readability

--- frontend/src/scripts/login.js
+++ frontend/src/scripts/login.js
@@ -1,27 +1,35 @@
-function onLoad(){
-    document.querySelector("#submitButton").addEventListener("click", submitLoginForm);
-}
+/**
+ * Handles the login form submission. Sends a POST request to the backend with the login form data.
+ */
+async function submitLoginForm(event){
+    event.preventDefault(); // Prevent the default form submission
 
-async function submitLoginForm(){
-
+    // OAuth2 requires FormData in the request body. Therefore, we create a FormData object from the login form
     const loginForm = document.forms["login"];
     const body = new FormData(loginForm);
 
-    const response = await fetch(
-        BACKENDURL + "auth/login/", {
-            method: "POST",
-            body: body,
-        });
+    const response = await fetch(BACKENDURL + "auth/login/", {
+        method: "POST",
+        body: body,
+    });
 
-    if (!response.ok) {
+    if (!response.ok) { // If login was not successful, display an error message
         document.getElementById("errorMessage").style.display = "block";
         return;
     }
 
+    // If login was successful, store the access token in the local storage and redirect to the chat overview
     const res = await response.json();
-
     localStorage.setItem("AuthToken", res["access_token"]);
     window.location.href = "/frontend/public/index.html";
 }
 
+/**
+ * Executed when the DOM is fully loaded. Adds an event listener to the submit button of the login form.
+ */
+function onLoad(){
+    document.querySelector("#submitButton").addEventListener("click", submitLoginForm);
+}
+
+// Entry point - Execute onLoad when the DOM is fully loaded
 window.addEventListener("DOMContentLoaded", onLoad);
