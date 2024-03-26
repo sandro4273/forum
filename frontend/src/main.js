9a44a0ee9ee1a5311ec55ad7032225ef50d36e93
LULL1 <160033744+LULL1@users.noreply.github.com>
2024-03-26 21:21:49 +0100

    fix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -71,7 +71,7 @@ async function updatePostList(posts){
         const { username, userRole, roleColor } = await getUserDetails(author_id);
 
         const postElement = document.createElement('p');
-        postElement.innerHTML = `<div class="post"><a href="${FRONTENDURL}frontend/public/post.html?id=${post["post_id"]}">${post["title"]}</a><div> ${username} <span style="color: ${roleColor}">(${userRole})</span></div></div>`;
+        postElement.innerHTML = `<div class="post"><a style="text-decoration: none;color:black" href="${FRONTENDURL}frontend/public/post.html?id=${post["post_id"]}">${post["title"]}</a><div> ${username} <span style="color: ${roleColor}">(${userRole})</span></div></div>`;
         postList.append(postElement);
     }
 }
