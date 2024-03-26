1dc240c751d7dafbbdeabacb8e1a8775ad62cfa8
LULL1 <160033744+LULL1@users.noreply.github.com>
2024-03-26 20:29:30 +0100

    postgrid

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -71,7 +71,7 @@ async function updatePostList(posts){
         const { username, userRole, roleColor } = await getUserDetails(author_id);
 
         const postElement = document.createElement('p');
-        postElement.innerHTML = `<a href="${FRONTENDURL}frontend/public/post.html?id=${post["post_id"]}">${post["title"]}</a> - ${username} <span style="color: ${roleColor}">(${userRole})</span>`;
+        postElement.innerHTML = `<div class="post"><a href="${FRONTENDURL}frontend/public/post.html?id=${post["post_id"]}">${post["title"]}</a><div> ${username} <span style="color: ${roleColor}">(${userRole})</span></div></div>`;
         postList.append(postElement);
     }
 }
