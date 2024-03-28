4f1394ce1922f7feda243654bd6825780cb7c60a
lucafluehler <131798379+lucafluehler@users.noreply.github.com>
2024-03-28 16:59:32 +0100

    comments

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from backend.api.api import api_router
+from fastapi import FastAPI  # FastAPI is the main framework used for the backend API
+from fastapi.middleware.cors import CORSMiddleware  # CORS is needed to allow requests from the frontend
+from backend.api.api import api_router  # API router which connects all the endpoints
 
 # In case of CORS error, add your local host to the list of origins
 origins = [
@@ -13,13 +13,14 @@ origins = [
 # Initialize FastAPI
 app = FastAPI()
 
+# Configure the CORS middleware
 app.add_middleware(CORSMiddleware,
                    allow_origins=origins,
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
 
-# Include API router
+# Include all routes from the api_router. This connects all endpoints defined in api.py
 app.include_router(api_router)
 
 if __name__ == "__main__":
