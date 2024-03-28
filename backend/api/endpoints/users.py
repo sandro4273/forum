df24fbfe0155acdaf8602c55b46eb70ef990b5f9
lucafluehler <131798379+lucafluehler@users.noreply.github.com>
2024-03-28 17:51:21 +0100

    comments

--- backend/api/endpoints/users.py
+++ backend/api/endpoints/users.py
@@ -129,6 +129,7 @@ async def ban_user(user_id: int, current_user_id: Annotated[int, Depends(get_cur
     """
     Bans a user with the given user_id.
     """
+
     # Verify if the current user can ban this user
     current_user_role = db.get_user_by_id(current_user_id).role
     user_role = db.get_user_by_id(user_id).role
@@ -146,6 +147,7 @@ async def promote_user_to_moderator(user_id: int, current_user_id: Annotated[int
     """
     Promotes a user with the given user_id to moderator.
     """
+
     # Verify if the current user can promote this user
     current_user_role = db.get_user_by_id(current_user_id).role
     user_role = db.get_user_by_id(user_id).role
@@ -163,6 +165,7 @@ async def promote_user_to_admin(user_id: int, current_user_id: Annotated[int, De
     """
     Promotes a user with the given user_id to admin.
     """
+
     # Verify if the current user can promote this user
     current_user_role = db.get_user_by_id(current_user_id).role
     user_role = db.get_user_by_id(user_id).role
@@ -180,6 +183,7 @@ async def demote_moderator_to_user(user_id: int, current_user_id: Annotated[int,
     """
     Demotes a moderator with the given user_id to user.
     """
+
     # Verify if the current user can demote this user
     current_user_role = db.get_user_by_id(current_user_id).role
     user_role = db.get_user_by_id(user_id).role
@@ -197,6 +201,7 @@ async def demote_admin_to_moderator(user_id: int, current_user_id: Annotated[int
     """
     Demotes an admin with the given user_id to moderator.
     """
+
     # Verify if the current user can demote this user
     current_user_role = db.get_user_by_id(current_user_id).role
     user_role = db.get_user_by_id(user_id).role
